7e5e1e799ae8b2edc3f4bc303f3f65be9da93b3a
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-07-11 15:05:03 +0200

    remove background from widget

--- app/src/main/java/com/sonnenstahl/caffeine/HomeCafeUi.kt
+++ app/src/main/java/com/sonnenstahl/caffeine/HomeCafeUi.kt
@@ -10,19 +10,17 @@ import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
-import androidx.glance.background
 import androidx.glance.color.ColorProvider
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
-import com.sonnenstahl.caffeine.ui.theme.Background
 import com.sonnenstahl.caffeine.ui.theme.PrimaryColor
 import com.sonnenstahl.caffeine.ui.theme.TextColor
 import kotlinx.coroutines.launch
 
 @Composable
 fun HomeCafeUi() {
-    val couroutine = rememberCoroutineScope()
+    val coroutine = rememberCoroutineScope()
     val context = LocalContext.current
     val caffeinated = Caffeine.isScreenOn.collectAsState()
     val buttonText = remember(caffeinated.value) {
@@ -37,7 +35,6 @@ fun HomeCafeUi() {
         modifier =
             GlanceModifier
                 .fillMaxSize()
-                .background(Background)
                 .clickable(actionStartActivity<MainActivity>()),
         verticalAlignment = Alignment.CenterVertically ,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -45,7 +42,7 @@ fun HomeCafeUi() {
         Button(
             text= buttonText,
             onClick = {
-                couroutine.launch {
+                coroutine.launch {
                     when (caffeinated.value) {
                         true -> Caffeine.decaf()
                         false -> Caffeine.caffeinate(context, 60)
